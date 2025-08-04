131b8e98e0ef0e79df915a5412719f7340d48d7a
Yunho Cho <yhjc2692@gmail.com>
2025-08-03 20:17:20 -0400

    Added wasm binaries as static asset

--- src/lib/components/WebcamPose.svelte
+++ src/lib/components/WebcamPose.svelte
@@ -12,9 +12,7 @@
 
 	onMount(async () => {
 		const createPoseLandmarker = async () => {
-			const vision = await FilesetResolver.forVisionTasks(
-				'https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm'
-			);
+			const vision = await FilesetResolver.forVisionTasks('/wasm');
 			poseLandmarker = await PoseLandmarker.createFromOptions(vision, {
 				baseOptions: {
 					modelAssetPath: `https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/1/pose_landmarker_lite.task`,
